cd14889cf26263282efff2066fb5b2e7df5df8fb
Влад Скороделов <vlad197431@gmail.com>
2021-05-09 01:35:13 +0300

    Добавьте файлы проекта.

---
+++ Spt и FB/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿
+namespace Spt_и_FB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(259, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "FB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(259, 34);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(375, 234);
+            this.textBox1.TabIndex = 1;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 53);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(12, 114);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(12, 175);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown3.TabIndex = 4;
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(12, 242);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown4.TabIndex = 5;
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(12, 307);
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDown5.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(551, 292);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 41);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "SPT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Вероятность прихода заявки(R)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Длительность решения задачи";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(247, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Величина короткой заявки (LK)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Количество заявок";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(230, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Максимальная длина заявки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 365);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.Button button2;
+        internal System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Spt и FB/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System.IO;
+using EPPlus;
+namespace Spt_и_FB
+{
+    public partial class Form1 : Form
+    {
+        static string path = Path.GetDirectoryName(Application.StartupPath)+ "excel.xlsx";
+        public static int Current_Column_FB = 2;
+        public static int Current_Column_SPT = 2;
+        public static FileInfo file = new FileInfo(@path);
+        ExcelPackage package = new ExcelPackage(@file);
+        public Form1()
+        {
+            
+            InitializeComponent();
+            if (file.Exists)
+            {
+                file.Delete();
+                file = new FileInfo(@path);
+            }
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("FB");
+            ExcelWorksheet worksheet2 = package.Workbook.Worksheets.Add("SPT");
+            for (int i = 1; i < 9; i++)
+            {
+                worksheet.Cells[i, 1].Style.WrapText = true;
+                worksheet2.Cells[i, 1].Style.WrapText = true;
+            }
+            worksheet.Cells["A1"].Value = "Количество заявок = ";
+            worksheet.Cells["A2"].Value = "Вероятность прихода заявки = ";
+            worksheet.Cells["A3"].Value = "Длина короткой заявки = ";
+            worksheet.Cells["A4"].Value = "Максимальная длина = ";
+            worksheet.Cells["A5"].Value = "Длительность процессорного кванта = ";
+            worksheet.Cells["A6"].Value = "Результаты: \r \nСумма длин всех заявок";
+            worksheet.Cells["A7"].Value = "Количество тактов = ";
+            worksheet.Cells["A8"].Value = "Время ожидания короткой заявки = ";
+
+            worksheet2.Cells["A1"].Value = "Количество заявок = ";
+            worksheet2.Cells["A2"].Value = "Вероятность прихода заявки = ";
+            worksheet2.Cells["A3"].Value = "Длина короткой заявки = ";
+            worksheet2.Cells["A4"].Value = "Максимальная длина = ";
+            worksheet2.Cells["A5"].Value = "Длительность процессорного кванта = ";
+            worksheet2.Cells["A6"].Value = "Результаты: \r \nСумма длин всех заявок";
+            worksheet2.Cells["A7"].Value = "Количество тактов = ";
+            worksheet2.Cells["A8"].Value = "Время ожидания короткой заявки = ";
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();//FB
+            Queue<int> queue = new Queue<int>();
+            int kol = (int)numericUpDown4.Value; // Количестово заявок
+            int ML = (int)numericUpDown5.Value; //максимальная длина заявки
+            double R = (double)numericUpDown1.Value; //вероятность прихода ненулевой заявки
+            int LK = (int)numericUpDown3.Value; // длина короткой заявки
+            int L = (int)numericUpDown2.Value; // длительность рещения задачи
+            int[] mas = new int[kol];
+            int count_short = 0, tact_count = 0, short_time = 0, null_request = 0;
+            Random rnd = new Random();
+            int Sum = 0;
+            for (int j = 0; j < kol; j++)
+            {
+                if (rnd.Next(100) < R)
+                {
+                    int temp = rnd.Next(ML);
+                    queue.Enqueue(temp);
+                    Sum += temp;
+                }
+                else
+                    queue.Enqueue(0);
+            }
+            while(queue.Count != 0)
+            {
+                if (queue.Peek() != 0)
+                {
+                    if(queue.Peek()<= LK)
+                    {
+                        count_short++;
+                        tact_count++;
+                        short_time += L - queue.Dequeue();
+                    }
+                    else
+                    {
+                        tact_count++;
+                        int temp = queue.Dequeue() - L;
+                        if (temp > 0)
+                            queue.Enqueue(temp);
+                    }
+                }
+                else 
+                {
+                    queue.Dequeue();
+                    null_request++;
+                }
+            }
+
+            float average = (float)short_time / (float)count_short;
+            textBox1.Text += '\n' + "Сумма длин всех заявок в очереди = " + Convert.ToString(Sum) + "\r";
+            textBox1.Text += '\n' + "Количество выполненных тактов = " +
+            Convert.ToString(tact_count) + "\n \r";
+            textBox1.Text += '\n' + "Среднее время ожидания одной короткой заявки = " + Convert.ToString(average) + " ";
+            //ExcelPackage package = new ExcelPackage(file);
+            ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+            //worksheet.Cells["A1"].Value = "Сумма длин всех заявок в очереди = ";
+            worksheet.Cells[1, Current_Column_FB].Value = Convert.ToString(kol);
+            worksheet.Cells[2, Current_Column_FB].Value = Convert.ToString(R);
+            worksheet.Cells[3, Current_Column_FB].Value = Convert.ToString(LK);
+            worksheet.Cells[4, Current_Column_FB].Value = Convert.ToString(ML);
+            worksheet.Cells[5, Current_Column_FB].Value = Convert.ToString(L);
+
+            worksheet.Cells[6, Current_Column_FB].Value = Convert.ToString(Sum);
+            worksheet.Cells[7, Current_Column_FB].Value = Convert.ToString(tact_count);
+            worksheet.Cells[8, Current_Column_FB].Value = Convert.ToString(average);
+            Current_Column_FB++;//переход к след. столбцу в excel
+            package.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            int kol = (int)numericUpDown4.Value; // Количестово заявок
+            int ML = (int)numericUpDown5.Value; //максимальная длина заявки
+            double R = (double)numericUpDown1.Value; //вероятность прихода ненулевой заявки
+            int LK = (int)numericUpDown3.Value; // длина короткой заявки
+            int L = (int)numericUpDown2.Value; // длительность рещения задачи
+            int[] mas = new int[kol];
+            Random rnd = new Random();
+            int count_short = 0, tact_count = 0, short_time = 0, null_request = 0;
+            int Sum = 0;
+            
+            for (int j = 0; j < kol; j++)
+            {
+                if (rnd.Next(100) < R)
+                    mas[j] = rnd.Next(ML);
+                else
+                    mas[j] = 0;
+                Sum += mas[j];
+            }
+            Array.Sort(mas);
+            for (int i = 0; i < kol; i++)
+            {
+                if (mas[i] > 0)
+                {
+                    if (mas[i] <= LK)
+                    {
+                        count_short++;
+                        tact_count++;
+                        short_time += L - mas[i];
+                        mas[i] = 0;
+                    }
+                    if (mas[i] > LK)
+                    {
+                        int current = mas[i];
+                        while (current >= 0)
+                        {
+                            current -= L;
+                            tact_count++;
+                        }
+                        mas[i] = 0;
+                    }
+                }
+                else
+                {
+
+                    null_request++;
+                }
+            }
+            float average = (float)short_time / (float)count_short;
+            textBox1.Text += '\n' + "Сумма длин всех заявок в очереди = " + Convert.ToString(Sum) + "\r";
+            textBox1.Text += '\n' + "Количество выполненных тактов = " +
+            Convert.ToString(tact_count) + "\n \r";
+            textBox1.Text += '\n' + "Среднее время ожидания одной короткой заявки = " + Convert.ToString(average) + " ";
+            // ExcelPackage package = new ExcelPackage(file);
+            ExcelWorksheet worksheet = package.Workbook.Worksheets[2];
+            //worksheet.Cells["A1"].Value = "Сумма длин всех заявок в очереди = ";
+            worksheet.Cells[1, Current_Column_SPT].Value = Convert.ToString(kol);
+            worksheet.Cells[2, Current_Column_SPT].Value = Convert.ToString(R);
+            worksheet.Cells[3, Current_Column_SPT].Value = Convert.ToString(LK);
+            worksheet.Cells[4, Current_Column_SPT].Value = Convert.ToString(ML);
+            worksheet.Cells[5, Current_Column_SPT].Value = Convert.ToString(L);
+
+            worksheet.Cells[6, Current_Column_SPT].Value = Convert.ToString(Sum);
+            worksheet.Cells[7, Current_Column_SPT].Value = Convert.ToString(tact_count);
+            worksheet.Cells[8, Current_Column_SPT].Value = Convert.ToString(average);
+            Current_Column_SPT++;//переход к след. столбцу в excel
+            package.Save();
+        }
+    } 
+}
